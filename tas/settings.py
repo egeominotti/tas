6ba46fc306833d5598c102d442c4d936d94d604a
egeominotti <egeominotti@gmail.com>
2021-07-23 01:32:43 +0200

    - fix timezone

--- tas/settings.py
+++ tas/settings.py
@@ -122,6 +122,8 @@ LANGUAGE_CODE = 'it-it'
 TIME_ZONE = 'Europe/Rome'
 USE_I18N = True
 USE_L10N = True
+USE_TZ = True
+
 
 STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
