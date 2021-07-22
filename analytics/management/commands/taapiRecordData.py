7e18a20a7960234d9d6430a55dc3e4792ec459f7
egeominotti <egeominotti@gmail.com>
2021-07-22 22:00:12 +0200

    - fix timezone

--- analytics/management/commands/taapiRecordData.py
+++ analytics/management/commands/taapiRecordData.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from time import sleep
-
+import threading
 import schedule
 from analytics.models import ExchangeRecord
 from django.core.management import BaseCommand
