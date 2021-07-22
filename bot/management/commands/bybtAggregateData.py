9ee9fb32a4c6b9a79f4d0f999e21ee9080c9e499
egeominotti <egeominotti@gmail.com>
2021-07-23 01:08:50 +0200

    - fix timezone

--- bot/management/commands/bybtAggregateData.py
+++ bot/management/commands/bybtAggregateData.py
@@ -1,14 +1,11 @@
 from django.core.management import BaseCommand
-from analytics.models import ByBt
-from datetime import datetime, timedelta
-import datetime
 import logging
 
 logger = logging.getLogger('main')
 
 
 class Command(BaseCommand):
-    help = 'Calcola la media degli indici per 1H,2H,4H,8H,12H,24H'
+    help = ''
 
     def handle(self, *args, **kwargs):
         pass
