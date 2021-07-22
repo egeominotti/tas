939f787b5b0c43f1806258249a5d1704eb45d039
egeominotti <egeominotti@gmail.com>
2021-07-22 23:07:45 +0200

    - fix timezone

--- analytics/management/commands/taapiRecordData.py
+++ analytics/management/commands/taapiRecordData.py
@@ -10,44 +10,47 @@ import logging
 logger = logging.getLogger('main')
 
 
+ticker = 'BTC/USDT'
+taapi = Taapi(ticker)
+listema = [5, 9, 10, 12, 24, 27, 42, 50, 60, 223]
+
+def scheduledTimeFrame(tf):
+    print(tf)
+    print(tf)
+    print(tf)
+
+    candle = taapi.candle(tf)
+    candletimestamp = datetime.fromtimestamp(candle['timestamp'])
+    candleopen = candle['open']
+    candlehigh = candle['high']
+    candlelow = candle['low']
+    candleclose = candle['close']
+    candlevolume = candle['volume']
+
+    dizema = {}
+    for j in listema:
+        ema = taapi.ema(j, tf)
+        dizema[j] = ema
+
+    ExchangeRecord.objects.create(
+        symbol=ticker,
+        tf=tf,
+        timestamp=candletimestamp,
+        open=candleopen,
+        high=candlehigh,
+        low=candlelow,
+        close=candleclose,
+        volume=candlevolume,
+        ema=dizema
+    )
+
 class Command(BaseCommand):
     help = 'Registra i dati di taapi'
 
     def handle(self, *args, **kwargs):
 
-        ticker = 'BTC/USDT'
-        taapi = Taapi(ticker)
-        listema = [5, 9, 10, 12, 24, 27, 42, 50, 60, 223]
-
-        def scheduledTimeFrame(tf):
-
-            candle =                taapi.candle(tf)
-            candletimestamp =       datetime.fromtimestamp(candle['timestamp'])
-            candleopen =            candle['open']
-            candlehigh =            candle['high']
-            candlelow =             candle['low']
-            candleclose =           candle['close']
-            candlevolume =          candle['volume']
-
-            dizema = {}
-            for j in listema:
-                ema = taapi.ema(j, tf)
-                dizema[j] = ema
-
-            ExchangeRecord.objects.create(
-                symbol = ticker,
-                tf = tf,
-                timestamp = candletimestamp,
-                open = candleopen,
-                high = candlehigh,
-                low = candlelow,
-                close = candleclose,
-                volume = candlevolume,
-                ema = dizema
-            )
-
-
         schedule.every(1).minutes.do(scheduledTimeFrame, tf='1m')
+        schedule.every(5).minutes.do(scheduledTimeFrame, tf='5m')
         schedule.every(15).minutes.do(scheduledTimeFrame, tf='15m')
         schedule.every(30).minutes.do(scheduledTimeFrame, tf='30m')
         schedule.every(60).minutes.do(scheduledTimeFrame, tf='1h')
@@ -60,3 +63,4 @@ class Command(BaseCommand):
 
         while True:
             schedule.run_pending()
+            sleep(1)
