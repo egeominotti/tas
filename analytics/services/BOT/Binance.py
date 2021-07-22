13236f3599824012babf8d6f5a2d200fd76b2309
egeominotti <egeominotti@gmail.com>
2021-07-23 01:17:21 +0200

    - fix timezone

--- analytics/services/BOT/Binance.py
+++ analytics/services/BOT/Binance.py
@@ -6,25 +6,23 @@ from binance.enums import *
 
 
 class Binance:
-
     client = Client(config('api_key_binance'), config('api_secret_binance'))
 
+    def __init__(self, symbol, leverage):
+        self.client.futures_change_leverage(symbol=symbol, marginType='ISOLATED', leverage=leverage)
 
-    def __init__(self, symbol, quantity, leverage):
-        self.client.futures_change_leverage(symbol=symbol, marginType='ISOLATED', leverage=5)
-
-    def buy(self):
-         self.client.futures_create_order(
+    def buy(self, quantity):
+        self.client.futures_create_order(
             symbol='BTCUSDT',
             side=SIDE_BUY,
             type=ORDER_TYPE_MARKET,
-            quantity=QUANTITY,
+            quantity=quantity,
         )
 
-    def sell(self):
+    def sell(self, quantity):
         self.client.futures_create_order(
             symbol='BTCUSDT',
             side=SIDE_SELL,
             type=ORDER_TYPE_MARKET,
-            quantity=QUANTITY,
+            quantity=quantity,
         )
