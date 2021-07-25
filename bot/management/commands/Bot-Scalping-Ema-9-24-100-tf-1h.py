7fcbee2c3eb08eb30c7a4ea4ada9d9bd839b7fd6
egeominotti <egeominotti@gmail.com>
2021-07-26 01:07:33 +0200

    - framework init

--- bot/management/commands/Bot-Scalping-Ema-9-24-100-tf-1h.py
+++ bot/management/commands/Bot-Scalping-Ema-9-24-100-tf-1h.py
@@ -141,6 +141,6 @@ class Command(BaseCommand):
                     long = False
 
                 telegram_bot_sendtext(
-                    "Time frame del bot: \n" + str(time_frame) + " - valore candela di acquisto: \n" + str(
-                        valueLong) + "\n - valore ultima candela ad un minuto:" + str(candle_close))
+                    "Time frame del bot: \n" + str(time_frame) + " - posizione aperta con valore: \n" + str(
+                        valueLong) + "\n - valore candela ad un minuto close:" + str(candle_close))
                 sleep(50)
