68043c4eb4f283a1edcee70a9d318aeb8f0897a0
egeominotti <egeominotti@gmail.com>
2021-07-26 18:39:45 +0200

    - framework init

--- backtest/management/commands/binanceKlines.py
+++ backtest/management/commands/binanceKlines.py
@@ -1,6 +1,8 @@
 import csv
+import time
 from time import sleep
 
+import numpy as np
 from django.core.management import BaseCommand
 from backtest.strategy.long.Scalping import StrategyTest
 from backtest.models import BackTest
@@ -8,7 +10,7 @@ from analytics.models import ExchangeRecord
 import pandas as pd
 import logging
 from dateutil import parser
-from binance import Client
+from binance import Client, BinanceSocketManager
 from decouple import config
 import btalib
 import talib
@@ -18,6 +20,9 @@ from talib import abstract, SMA
 import numpy
 from numpy import genfromtxt
 import backtrader as bt
+import ccxt
+
+from datetime import datetime
 
 logger = logging.getLogger('main')
 
@@ -25,38 +30,18 @@ client = Client(config('API_KEY_BINANCE'), config('API_SECRET_BINANCE'))
 
 
 # https://mrjbq7.github.io/ta-lib/func_groups/overlap_studies.html
-def get_df(symbol):
-    client_columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume',
-                      'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore']
-    data = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1MINUTE, "135 mins ago UTC")
-    df = pd.DataFrame(data,
-                      columns=['open_time', 'open', 'high', 'low', 'close', 'volume'],
-                      dtype='float64')
-
-    return df
-
-
-class RSIStrategy(bt.Strategy):
-
-    def __init__(self):
-        self.rsi = bt.talib.RSI(self.data, period=14)
-
-    def next(self):
-        if self.rsi < 30 and not self.position:
-            self.buy(size=1)
-        if self.rsi < 70 and not self.position:
-            self.buy(size=1)
-
 
 class Command(BaseCommand):
     help = 'Backtesting strategy scalping'
 
     def handle(self, *args, **kwargs):
+
         file = 'backtest/file/daily.csv'
 
         csvfile = open(file, 'w', newline='')
         candlestick_write = csv.writer(csvfile, delimiter=',')
-        candlesticks = client.get_historical_klines('BTCUSDT', '1h', "1 Jan, 2019", "1 Jan, 2021")
+        candlesticks = client.get_historical_klines('BTCUSDT', '1h', "1 Jan, 2021", "30 Jan, 2021")
+
 
         candlestick_processed = []
         for data in candlesticks:
@@ -75,10 +60,8 @@ class Command(BaseCommand):
 
         my_data = genfromtxt(file, delimiter='')
         close = my_data[:, 4]
+        print(close)
 
         ema9 = talib.EMA(close, timeperiod=9)
         ema24 = talib.EMA(close, timeperiod=24)
         ema50 = talib.EMA(close, timeperiod=50)
-        print(ema9)
-        print(ema24)
-        print(ema50)
