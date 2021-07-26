ebe3abbfe0225c294ca7dc0f35856c257188010c
egeominotti <egeominotti@gmail.com>
2021-07-27 00:25:30 +0200

    - framework init

--- backtest/management/commands/LONG-Scalping-Ema-9-24-50-tf-1h.py
+++ backtest/management/commands/LONG-Scalping-Ema-9-24-50-tf-1h.py
@@ -2,6 +2,7 @@ from bisect import bisect
 from datetime import datetime
 
 import numpy as np
+import pandas
 from django.core.management import BaseCommand
 from backtest.strategy.long.Scalping import Strategy
 import logging
@@ -56,7 +57,16 @@ class StrategyLongScalpingEMA(Strategy):
 
         return computed_data
 
-    def logic(self, item, diz) -> None:
+
+
+    def generate_signals(self) -> dict:
+        diz = {}
+        for item in self.computed_data():
+            if item is not None:
+                self.logic_signals(item, diz)
+        return diz
+
+    def logic_signals(self, item, diz) -> None:
 
         """
         Scrivere la logica qui
@@ -70,93 +80,61 @@ class StrategyLongScalpingEMA(Strategy):
         Fine logica
         """
 
-    def generate_signals(self) -> dict:
-        diz = {}
-        for item in self.computed_data():
-            if item is not None:
-                self.logic(item, diz)
-        return diz
+    def logic_stop_loss(self, candle_close_entry, candle_iterate, stop_loss):
+        if candle_close_entry < candle_iterate * stop_loss:
+            return True
+        return False
 
-    # def check_entry(self, take_profit, stop_loss) -> None:
-    #
-    #     computed_data = self.computed_data()
-    #     signals = self.generate_signals()
-    #
-    #     print(len(signals))
-    #     print(len(computed_data))
-    #     """
-    #     Scrivere la logica stop_loss o take_profit
-    #     """
-    #
-    #     return None
+    def logic_takeprofit(self, candle_close_entry, candle_iterate, take_profit):
+        if candle_close_entry < candle_iterate * take_profit:
+            return True
+        return False
+
+    def check_entry(self, take_profit, stop_loss):
+
+        counterTP = 0
+        counterSL = 0
+
+        signals = self.generate_signals()
+        computed_bars = self.computed_data()
+
+        computed_bars_dataframe = pandas.DataFrame(computed_bars,
+                                                   columns=['unix', 'timestamp', 'open', 'high', 'low', 'close',
+                                                            'volume'])
+
+        for k, v in signals.items():
+            tf = computed_bars_dataframe.loc[computed_bars_dataframe['timestamp'] > v['timestamp']]
+            for j, n in tf.iterrows():
+
+                if self.logic_takeprofit(v['close'], n['close'], take_profit) is True:
+                    counterTP += 1
+                    break
+
+                if self.logic_stop_loss(v['close'], n['close'], stop_loss) is True:
+                    counterSL += 1
+                    break
+
+        print("-----------------------")
+        print("ENTRY: " + str(len(signals)))
+        print("TAKE PROFIT: " + str(counterTP))
+        print("STOP LOSS: " + str(counterSL))
+
+        profit_ratio = counterTP / len(signals) * 100
+        loss_ratio = counterSL / len(signals) * 100
+
+        print("PROFIT RATIO: " + str(int(profit_ratio)) + "%")
+        print("LOSS RATIO: " + str(int(loss_ratio)) + "%")
+        print("-----------------------")
 
 
 class Command(BaseCommand):
     help = 'Backtesting strategy scalping'
 
     def handle(self, *args, **kwargs):
-
-        TAKE_PROFIT = 1.02
-        STOP_LOSS = 0.98
-        RATIO = 1.00005
-
         now = datetime.now().strftime("%d %b, %Y")
         client = Client(config('API_KEY_BINANCE'), config('API_SECRET_BINANCE'))
 
-        klines = client.get_historical_klines('RVNUSDT', Client.KLINE_INTERVAL_1HOUR, "17 Aug, 2017", now)
+        klines = client.get_historical_klines('BTCUSDT', Client.KLINE_INTERVAL_1HOUR, "17 Aug, 2017", now)
 
-        st = StrategyLongScalpingEMA(klines)
-        signals = st.generate_signals()
-        computed_bars = st.computed_data()[0]
-
-        # print(len(computed_bars))
-        # print(len(signals))
-        for k, v in signals.items():
-            val = computed_bars[k]
-            print(val)
-
-        # for time_candle, candle_close in dizEntry.items():
-        #     pandasTimeFrmae = df.loc[df['timestamp'] > time_candle]
-        #     for k, v in pandasTimeFrmae.iterrows():
-        #
-        #         take_profit = scalping_test.take_profit(v['close'], candle_close)
-        #         stop_loss = scalping_test.stop_loss(v['close'], candle_close)
-        #
-        #         if take_profit is True:
-        #             counterTp += 1
-        #             BackTest.objects.create(
-        #                 algorithm='LONG-Scalping-Ema-9-24-50-tf-1h',
-        #                 entry_candle=candle_close,
-        #                 entry_candle_date=time_candle,
-        #                 candle_take_profit=v['close'],
-        #                 candle_take_profit_date=v['timestamp'],
-        #                 take_profit=True,
-        #             )
-        #
-        #             break
-        #
-        #         if stop_loss is True:
-        #             counterSl += 1
-        #
-        #             BackTest.objects.create(
-        #                 algorithm='LONG-Scalping-Ema-9-24-50-tf-1h',
-        #                 entry_candle=candle_close,
-        #                 entry_candle_date=time_candle,
-        #                 candle_stop_loss=v['close'],
-        #                 candle_stop_loss_date=v['timestamp'],
-        #                 stop_loss=True,
-        #             )
-        #
-        #             break
-        #
-        # print("-----------------------")
-        # print("ENTRY: " + str(len(dizEntry)))
-        # print("TAKE PROFIT: " + str(counterTp))
-        # print("STOP LOSS: " + str(counterSl))
-        #
-        # profit_ratio = counterTp / len(dizEntry) * 100
-        # loss_ratio = counterSl / len(dizEntry) * 100
-        #
-        # print("PROFIT RATIO: " + str(int(profit_ratio)) + "%")
-        # print("LOSS RATIO: " + str(int(loss_ratio)) + "%")
-        # print("-----------------------")
+        st = StrategyLongScalpingEMA(klines=klines)
+        st.check_entry(take_profit=1.02, stop_loss=0.98)
