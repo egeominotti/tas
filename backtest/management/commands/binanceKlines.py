8d8da3c9715dd0d0ebf3b7a99be98783ddb3b82d
egeominotti <egeominotti@gmail.com>
2021-07-26 20:01:27 +0200

    - framework init

--- backtest/management/commands/binanceKlines.py
+++ backtest/management/commands/binanceKlines.py
@@ -59,6 +59,10 @@ class Command(BaseCommand):
           ]
         ]
         """
+
+        # listema = [5, 7, 9, 10, 12, 24, 27, 42, 50, 60, 100, 200, 223, 365]
+        # Timeframe 1m, 15m, 30m, 1h, 2h, 4h, 8h, 12h, 1D,3D,1
+
         klines = client.get_historical_klines('BTCUSDT', Client.KLINE_INTERVAL_1MINUTE, "26 Jul, 2018", "26 Jul, 2021")
 
         time = [entry[0] / 1000 for entry in klines]
@@ -69,15 +73,27 @@ class Command(BaseCommand):
         volume = [float(entry[5]) for entry in klines]
 
         close_array = np.asarray(close)
+        ema5 = ta.EMA(close_array, timeperiod=5)
+        ema7 = ta.EMA(close_array, timeperiod=7)
         ema9 = ta.EMA(close_array, timeperiod=9)
+        ema10 = ta.EMA(close_array, timeperiod=10)
+        ema12 = ta.EMA(close_array, timeperiod=12)
+        ema24 = ta.EMA(close_array, timeperiod=24)
+        ema27 = ta.EMA(close_array, timeperiod=27)
+        ema42 = ta.EMA(close_array, timeperiod=42)
         ema50 = ta.EMA(close_array, timeperiod=50)
-        ema100 = ta.RSI(close_array, timeperiod=100)
+        ema60 = ta.EMA(close_array, timeperiod=60)
+        ema100 = ta.EMA(close_array, timeperiod=100)
+        ema200 = ta.EMA(close_array, timeperiod=200)
+        ema223 = ta.EMA(close_array, timeperiod=223)
+        ema365 = ta.EMA(close_array, timeperiod=365)
         # upperband, middleband, lowerband = ta.BBANDS(close, timeperiod=5, nbdevup=2, nbdevdn=2, matype=0)
 
         diz = {}
         lenght = len(time)
         for i in range(lenght):
-            diz = {
+
+            diz[time[i]] = {
                 'unix': time[i],
                 'timestamp': datetime.fromtimestamp(time[i]),
                 'open': open[i],
@@ -85,11 +101,23 @@ class Command(BaseCommand):
                 'low': low[i],
                 'close': close[i],
                 'volume': volume[i],
+                'ema5': ema5[i],
+                'ema7': ema7[i],
                 'ema9': ema9[i],
+                'ema10': ema10[i],
+                'ema12': ema12[i],
+                'ema24': ema24[i],
+                'ema27': ema27[i],
+                'ema42': ema42[i],
                 'ema50': ema50[i],
-                'ema100': ema100[i]
+                'ema60': ema60[i],
+                'ema100': ema100[i],
+                'ema200': ema200[i],
+                'ema223': ema223[i],
+                'ema365': ema365[i]
             }
-            print(diz)
+        print(diz)
+
         # for i in xrange(len(my_lis)):
         #     open_time = open_time[k]
         #     close = close[k]
