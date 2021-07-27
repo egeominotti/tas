3fd731abd501967ea620f510d6e51702a78f818a
egeominotti <egeominotti@gmail.com>
2021-07-27 20:33:09 +0200

    - framework init

--- bot/management/commands/BotScalping5m.py
+++ bot/management/commands/BotScalping5m.py
@@ -29,13 +29,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
 
-        TAKE_PROFIT = 1.98
-        STOP_LOSS = 0.98
-        RATIO = 1.009
-        time_frame = '1m'
-        QUANTITY = 0.004
+        take_profit = 1.02
+        stop_loss = 0.98
+        ratio = 1.0098
+        time_frame = '5m'
+        quantity = 0.004
 
-        LIVE = False
+        live = False
         open_position = False
 
         taapi = Taapi('BTC/USDT')
@@ -44,7 +44,6 @@ class Command(BaseCommand):
 
         telegram_bot_sendtext("\n")
         telegram_bot_sendtext("\n")
-
 
         txt = "BOT: Started"
         telegram_bot_sendtext(txt)
@@ -67,12 +66,12 @@ class Command(BaseCommand):
                     'lowerband': bbands.get('valueLowerBand')
                 }
 
-                if scalping_5m_rsi_bollinger(item, RATIO):
+                if scalping_5m_rsi_bollinger(item, ratio):
 
                     s0 = "Time frame: " + time_frame
                     s1 = "Compro al prezzo: " + str(candle_close)
-                    s2 = "TP: " + str(candle_close * TAKE_PROFIT)
-                    s3 = "SL: " + str(candle_close * STOP_LOSS)
+                    s2 = "TP: " + str(candle_close * take_profit)
+                    s3 = "SL: " + str(candle_close * stop_loss)
 
                     telegram_bot_sendtext(s0 + "\n" + s1 + "\n" + s2 + "\n" + s3)
 
@@ -82,12 +81,12 @@ class Command(BaseCommand):
                     print(s3)
                     print("---------------------------------------------------")
 
-                    if LIVE:
+                    if live:
                         client.futures_create_order(
                             symbol='BTCUSDT',
                             side=SIDE_BUY,
                             type=ORDER_TYPE_MARKET,
-                            quantity=QUANTITY,
+                            quantity=quantity,
                         )
 
                     open_position_value = candle_close
@@ -110,32 +109,32 @@ class Command(BaseCommand):
                 }
                 print(item)
 
-                if takeprofit_scalping_5m_rsi_bollinger(open_position_value, candle_close, TAKE_PROFIT, item):
+                if takeprofit_scalping_5m_rsi_bollinger(open_position_value, candle_close, take_profit, item):
 
-                    print("TAKE_PROFIT: " + str(open_position_value * TAKE_PROFIT))
-                    telegram_bot_sendtext("TAKE_PROFIT: " + str(open_position_value * TAKE_PROFIT))
+                    print("TAKE_PROFIT: " + str(open_position_value * take_profit))
+                    telegram_bot_sendtext("TAKE_PROFIT: " + str(open_position_value * take_profit))
 
-                    if LIVE:
+                    if live:
                         client.futures_create_order(
                             symbol='BTCUSDT',
                             side=SIDE_SELL,
                             type=ORDER_TYPE_MARKET,
-                            quantity=QUANTITY,
+                            quantity=quantity,
                         )
 
                     break
 
-                if stoploss_scalping_5m_rsi_bollinger(open_position_value, candle_close, STOP_LOSS):
+                if stoploss_scalping_5m_rsi_bollinger(open_position_value, candle_close, stop_loss):
 
-                    print("STOP LOSS: " + str(open_position_value * STOP_LOSS))
-                    telegram_bot_sendtext("STOP LOSS: " + str(open_position_value * STOP_LOSS))
+                    print("STOP LOSS: " + str(open_position_value * stop_loss))
+                    telegram_bot_sendtext("STOP LOSS: " + str(open_position_value * stop_loss))
 
-                    if LIVE:
+                    if live:
                         client.futures_create_order(
                             symbol='BTCUSDT',
                             side=SIDE_SELL,
                             type=ORDER_TYPE_MARKET,
-                            quantity=QUANTITY,
+                            quantity=quantity,
                         )
 
                     break
