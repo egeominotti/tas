53e92fffa3a6095c6b55aba287d392f8c4c41344
egeominotti <egeominotti@gmail.com>
2021-07-27 17:38:04 +0200

    - framework init

--- backtest/strategy/long/strategy_long.py
+++ backtest/strategy/long/strategy_long.py
@@ -16,3 +16,25 @@ def logic_takeprofit(candle_close_entry, signal_candle_close, take_profit, item)
     if candle_close_entry > signal_candle_close * take_profit:
         return True
     return False
+
+
+def scalping_5m_rsi_bollinger(item, ratio) -> bool:
+    ratio_value = item['middleband'] / item['lowerband']
+    if ratio_value >= ratio:
+        if item['rsi'] > 30:
+            return True
+    return False
+
+
+def stoploss_scalping_5m_rsi_bollinger(candle_close_entry, signal_candle_close, stop_loss, item) -> bool:
+    if candle_close_entry < signal_candle_close * stop_loss:
+        return True
+    return False
+
+
+def takeprofit_scalping_5m_rsi_bollinger(candle_close_entry, signal_candle_close, take_profit, item) -> bool:
+    middleband_flag = item['middleband'] * 1.011
+
+    if candle_close_entry >= middleband_flag or item['close'] > signal_candle_close * take_profit:
+        return True
+    return False
