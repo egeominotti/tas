4676ef0b91c73826cfdaa83401e0b7f7ef677af1
egeominotti <egeominotti@gmail.com>
2021-07-27 05:17:27 +0200

    - framework init

--- backtest/management/commands/Backtest.py
+++ backtest/management/commands/Backtest.py
@@ -33,7 +33,7 @@ class Command(BaseCommand):
 
         crypto = ['BTCUSDT', 'ETHUSDT', 'ADAUSDT', 'MATICUSDT', 'BNBUSDT', 'CHZUSDT', 'VETUSDT', 'CAKEUSDT', 'AVAUSDT',
                   'DOTUSDT', 'SOLUSDT', 'TRXUSDT', 'TFUELUSDT', 'BTTUSDT']
-        time_frame = ['4h', '8h', '12h', '1d', '3d', '1w', '1M']
+        time_frame = ['15m', '30m', '1h', '4h', '8h', '12h', '1d', '3d', '1w', '1M']
 
         for k in crypto:
             for tf in time_frame:
