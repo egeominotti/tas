5261decdcc1186f537a88743e5e99733c93d0829
egeominotti <egeominotti@gmail.com>
2021-07-27 04:52:07 +0200

    - framework init

--- backtest/management/commands/Backtest.py
+++ backtest/management/commands/Backtest.py
@@ -37,7 +37,7 @@ class Command(BaseCommand):
 
         for k in crypto:
             for time in time_frame:
-                klines = client.get_historical_klines(k, time, "17 Aug, 2017", now)
+                klines = client.get_historical_klines(k, time, "17 Aug, 2019", now)
 
                 st = LongStrategyScalping_EMA_9_24_100(klines=klines, ratio=1.00005)
                 signals = st.generate_signals()
