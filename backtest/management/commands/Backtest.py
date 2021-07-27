89da1a5ca38ee53a31672966f72cf57e4fd72523
egeominotti <egeominotti@gmail.com>
2021-07-27 17:24:09 +0200

    - framework init

--- backtest/management/commands/Backtest.py
+++ backtest/management/commands/Backtest.py
@@ -27,7 +27,7 @@ class Command(BaseCommand):
                 return True
             return False
 
-        crypto = 'BTCUSDT'
+        crypto = 'ETHUSDT'
         RATIO = 1.00005
         TAKE_PROFIT = 1.021
         STOP_LOSS = 0.9845
