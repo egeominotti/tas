c72b0c4a51934029005495d7409d6c8c31445ba8
egeominotti <egeominotti@gmail.com>
2021-07-27 16:00:47 +0200

    - framework init

--- backtest/management/commands/Backtest.py
+++ backtest/management/commands/Backtest.py
@@ -16,8 +16,7 @@ class Command(BaseCommand):
         now = datetime.now().strftime("%d %b, %Y")
         client = Client(config('API_KEY_BINANCE'), config('API_SECRET_BINANCE'))
 
-        crypto = ['BTCUSDT']
-        time_frame = ['1h']
+
 
         def logic_signals(item) -> bool:
             ratio_value = item['ema9'] / item['ema24']
@@ -25,26 +24,28 @@ class Command(BaseCommand):
                 return True
             return False
 
-        def logic_stop_loss(candle_close_entry, signal_candle_close, stop_loss, current_item) -> bool:
+        def logic_stop_loss(candle_close_entry, signal_candle_close, stop_loss, item) -> bool:
             if candle_close_entry < signal_candle_close * stop_loss:
                 return True
             return False
 
-        def logic_takeprofit(candle_close_entry, signal_candle_close, take_profit, current_item) -> bool:
+        def logic_takeprofit(candle_close_entry, signal_candle_close, take_profit, item) -> bool:
             if candle_close_entry > signal_candle_close * take_profit:
                 return True
             return False
 
+        crypto = ['BTCUSDT']
+        time_frame = ['1h']
         for k in crypto:
             for tf in time_frame:
                 klines = client.get_historical_klines(k, tf, "17 Aug, 2020", now)
 
                 st = StrategyChecker(klines=klines, ratio=1.00005)
-                pf = PortfolioChecker(func_stop_loss=logic_stop_loss,
-                                      func_take_profit=logic_takeprofit,
-                                      time_frame=tf,
-                                      symbol=k,
-                                      klines=klines,
-                                      signals=st.add_strategy(logic_signals),
-                                      take_profit=1.021,
-                                      stop_loss=0.9845)
+                PortfolioChecker(func_stop_loss=logic_stop_loss,
+                                 func_take_profit=logic_takeprofit,
+                                 time_frame=tf,
+                                 symbol=k,
+                                 klines=klines,
+                                 signals=st.add_strategy(logic_signals),
+                                 take_profit=1.021,
+                                 stop_loss=0.9845)
