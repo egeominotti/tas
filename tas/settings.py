dae9ac8be9ea87d358cb828aa047e313e7e4e72e
egeominotti <egeominotti@gmail.com>
2021-07-28 23:06:29 +0200

    - dinamically bot

--- tas/settings.py
+++ tas/settings.py
@@ -125,12 +125,11 @@ USE_I18N = True
 USE_L10N = True
 #USE_TZ = True
 
-STATIC_URL = '/staticfiles/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "tas/static")]
 
-
-MEDIA_URL = '/mediafiles/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
