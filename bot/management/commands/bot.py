92764cbe989e2a1849c236fea633e16c7dbbc23c
egeominotti <egeominotti@gmail.com>
2021-07-28 04:08:34 +0200

    - dinamically bot

--- bot/management/commands/bot.py
+++ bot/management/commands/bot.py
@@ -45,24 +45,23 @@ class Command(BaseCommand):
         # bot.setexchange('BTCUSDT', quantity, 1)
         # bot.run(50)
 
-        while True:
-            indicators = []
-            qs = Bot.objects.all()
-            for k in qs:
-                for i in k.indicators.all():
-                    indicators.append(i.indicator)
+        indicators = []
+        qs = Bot.objects.all()
+        for k in qs:
+            for i in k.indicators.all():
+                indicators.append(i.indicator)
 
-                bot = TradinBot(
-                    symbol=k.symbol_taapi,
-                    time_frame=k.strategy.time_frame,
-                    ratio=k.strategy.ratio,
-                    take_profit=k.strategy.take_profit,
-                    stop_loss=k.strategy.stop_loss,
-                    func_entry=scalping_5m_rsi_bollinger,
-                    func_stop_loss=stoploss_scalping_5m_rsi_bollinger,
-                    func_take_profit=takeprofit_scalping_5m_rsi_bollinger,
-                    indicator=indicators,
-                    # ema_interval=ema_interval,
-                )
-                bot.setexchange(k.symbol_exchange, k.quantity_investement, k.leverage)
-                bot.run(k.sleep_run, k.sleep_profitloss)
+            bot = TradinBot(
+                symbol=k.symbol_taapi,
+                time_frame=k.strategy.time_frame,
+                ratio=k.strategy.ratio,
+                take_profit=k.strategy.take_profit,
+                stop_loss=k.strategy.stop_loss,
+                func_entry=eval(k.strategy.logic_entry_function),
+                func_stop_loss=eval(k.strategy.logic_stoploss_function),
+                func_take_profit=eval(k.strategy.logic_takeprofit_function),
+                indicator=indicators,
+                # ema_interval=ema_interval,
+            )
+            bot.setexchange(k.symbol_exchange, k.quantity_investement, k.leverage)
+            bot.run(k.sleep_run, k.sleep_profitloss)
