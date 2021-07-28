e02f92afdf65b5f951038c6715b8c7f1452a5314
egeominotti <egeominotti@gmail.com>
2021-07-28 03:22:46 +0200

    - dinamically bot

--- bot/admin.py
+++ bot/admin.py
@@ -1,11 +1,32 @@
 from django.contrib import admin
-from bot.models import Bot
+from bot.models import Bot, Indicator, BinanceAccount
 
 
 class BotAdmin(admin.ModelAdmin):
     search_fields = ['tf']
     list_per_page = 50
     ordering = ('id',)
+    list_display = ('id', 'name', 'status', 'time_frame', 'ratio', 'take_profit', 'stop_loss', 'logic_entry_function',
+                    'logic_takeprofit_function', 'logic_stoploss_function', 'binance_account')
+    exclude = ['flgEnable', ]
+
+
+class IndicatorAdmin(admin.ModelAdmin):
+    search_fields = ['tf']
+    list_per_page = 50
+    ordering = ('id',)
+    list_display = ('id', 'indicator',)
+    exclude = ['flgEnable', ]
+
+
+class BinanceAccountAdmin(admin.ModelAdmin):
+    search_fields = ['tf']
+    list_per_page = 50
+    ordering = ('id',)
+    list_display = ('id', 'user', 'api_key', 'api_secret')
+    exclude = ['flgEnable', ]
 
 
 admin.site.register(Bot, BotAdmin)
+admin.site.register(Indicator, IndicatorAdmin)
+admin.site.register(BinanceAccount, BinanceAccountAdmin)
--- bot/models.py
+++ bot/models.py
@@ -1,8 +1,36 @@
+from django.contrib.auth.models import User
 from django.db import models
 from analytics.models import CommonTrait
+from django.conf import settings
+
+BOT_STATUS = (
+    ('DISABLE', 'DISABLE'),
+    ('ENABLE', 'ENABLE'),
+    ('RUNNING', 'RUNNING'),
+)
+
+
+class BinanceAccount(CommonTrait):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    api_key = models.CharField(max_length=200, blank=False, null=False)
+    api_secret = models.CharField(max_length=200, blank=False, null=False)
+
+    def __str__(self):
+        if self.user is not None:
+            return str(self.user)
+
+class Indicator(CommonTrait):
+    indicator = models.CharField(max_length=15, blank=False)
+
+    def __str__(self):
+        if self.indicator is not None:
+            return str(self.indicator)
 
 
 class Bot(CommonTrait):
+    name = models.CharField(max_length=100, blank=False)
+    status = models.CharField(max_length=50, choices=BOT_STATUS, default=BOT_STATUS[0][0],
+                              blank=False, null=False)
     symbol_taapi = models.CharField(max_length=15, blank=False)
     symbol_exchange = models.CharField(max_length=10, blank=False)
     time_frame = models.CharField(max_length=10, blank=False)
@@ -12,10 +40,16 @@ class Bot(CommonTrait):
     sleep_run = models.IntegerField(default=0, blank=True)
     sleep_profitloss = models.IntegerField(default=0, blank=True)
     # indicator = ['candle', 'rsi', 'bbands', 'ema', 'stoch']
-    indicators = models.JSONField(blank=True, null=True)
-    # ema_interval = ['10', '20', '50', '11']
-    ema_interval = models.JSONField(blank=True, null=True)
-    quantity_investement = models.IntegerField(default=0, blank=True)
+    #ema_interval = ['10', '20', '50', '11']
+    # ema_interval = models.TEX(blank=True, null=True)
+    indicators = models.ManyToManyField(Indicator)
+    quantity_investement = models.FloatField(default=0, blank=False)
+    live = models.BooleanField(default=False)
+    binance_account = models.ForeignKey(BinanceAccount, on_delete=models.SET_NULL, null=True, blank=True)
     logic_entry_function = models.CharField(max_length=100, blank=False)
     logic_takeprofit_function = models.CharField(max_length=100, blank=False)
     logic_stoploss_function = models.CharField(max_length=100, blank=False)
+
+    def __str__(self):
+        if self.name is not None:
+            return str(self.name)
