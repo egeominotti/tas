5d3f5851fb506d0e43839a123983aa122212b0ab
egeominotti <egeominotti@gmail.com>
2021-07-29 01:05:17 +0200

    - dinamically bot

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,18 @@ then
     echo "Waiting for postgres..."
 
     while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 5
+      sleep 0.1
     done
 
     echo "PostgreSQL started"
 fi
 
+sleep 5
 
 python3 manage.py reset_schema --noinput
-python3 manage.py migrate --noinput
+
+sleep 1
+
 python3 manage.py dbrestore --noinput
 python3 manage.py migrate --noinput
 pip3 install -r requirements.txt
