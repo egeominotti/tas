25f50cd7762ffa9740b0e8178ed4fe3ca77450d8
egeominotti <egeominotti@gmail.com>
2021-07-30 00:32:21 +0200

    - main

--- deploy_update.sh
+++ deploy_update.sh
@@ -4,7 +4,7 @@ git pull
 pip3 install -r requirements.txt
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
-sudo systemctl restart redis
-sudo systemctl restart gunicorn
-sudo systemctl restart nginx
+systemctl restart redis
+systemctl restart gunicorn
+systemctl restart nginx
 source venv/bin/activate
