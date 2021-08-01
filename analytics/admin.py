ccd6b8c1c3f3e6424817131cf44fa963bc8ab7ee
egeominotti <egeominotti@gmail.com>
2021-08-01 22:21:45 +0200

    - removed binance api from env.dev

--- analytics/admin.py
+++ analytics/admin.py
@@ -20,7 +20,7 @@ class ExchangeRecordAdmin(admin.ModelAdmin):
 
 class TradeCheckerAdmin(admin.ModelAdmin):
     search_fields = ['tf']
-    list_display = ['created_at', 'updated_at', 'symbol', 'time_frame', 'long', 'trade_long', 'short', 'trade_short']
+    list_display = ['symbol', 'created_at', 'updated_at', 'time_frame', 'long', 'trade_long', 'short', 'trade_short']
     list_per_page = 50
     ordering = ('id',)
 
