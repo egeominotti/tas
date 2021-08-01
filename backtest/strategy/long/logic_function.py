d02cd96cb3015ccab08564c5abbc68e27a4b671e
egeominotti <egeominotti@gmail.com>
2021-08-01 21:40:18 +0200

    - removed binance api from env.dev

--- backtest/strategy/long/logic_function.py
+++ backtest/strategy/long/logic_function.py
@@ -177,9 +177,36 @@ def logic_entry_ema8_13_21_34(item, bot=False):
         """
         Casistica usata dal backtesting
         """
-        now = item['time_frame']
-        now_prev = item['time_frame'] - relativedelta(minutes=15)
-        print(now)
+        now_prev = None
+        now = item['timestamp']
+
+        if item['time_frame'] == '1m':
+            now_prev = item['timestamp'] - relativedelta(minutes=1)
+        if item['time_frame'] == '5m':
+            now_prev = item['timestamp'] - relativedelta(minutes=5)
+        if item['time_frame'] == '15m':
+            now_prev = item['timestamp'] - relativedelta(minutes=15)
+        if item['time_frame'] == '30m':
+            now_prev = item['timestamp'] - relativedelta(minutes=30)
+        if item['time_frame'] == '15m':
+            now_prev = item['timestamp'] - relativedelta(minutes=15)
+        if item['time_frame'] == '1h':
+            now_prev = item['timestamp'] - relativedelta(hours=1)
+        if item['time_frame'] == '2h':
+            now_prev = item['timestamp'] - relativedelta(hours=2)
+        if item['time_frame'] == '4h':
+            now_prev = item['timestamp'] - relativedelta(hours=4)
+        if item['time_frame'] == '8h':
+            now_prev = item['timestamp'] - relativedelta(hours=8)
+        if item['time_frame'] == '12h':
+            now_prev = item['timestamp'] - relativedelta(hours=12)
+        if item['time_frame'] == '1d':
+            now_prev = item['timestamp'] - relativedelta(days=1)
+        if item['time_frame'] == '3d':
+            now_prev = item['timestamp'] - relativedelta(days=3)
+        if item['time_frame'] == '1M':
+            now_prev = item['timestamp'] - relativedelta(months=1)
+
         print(now_prev)
         qs = Importer.objects.filter(symbol=item['symbol'], tf=item['time_frame'], timestamp__range=[now_prev, now])
         print(qs)
