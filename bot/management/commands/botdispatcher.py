93502dae3eaddb2db303c1b3a9579612472170ff
egeominotti <egeominotti@gmail.com>
2021-08-02 05:47:47 +0200

    - removed binance api from env.dev

--- bot/management/commands/botdispatcher.py
+++ bot/management/commands/botdispatcher.py
@@ -7,6 +7,7 @@ from analytics.models import TrendChecker
 from bot.models import Bot, BotLogger
 from strategy.models import Strategy
 import logging
+from django_q.brokers import get_broker
 
 logger = logging.getLogger('main')
 
@@ -15,29 +16,42 @@ class Command(BaseCommand):
     help = 'DispatcherBot'
 
     def handle(self, *args, **kwargs):
-
+        broker = get_broker()
         while True:
-            qs = Strategy.objects.all()
-            usr = User.objects.all()
-            for user in usr:
-                print(user)
-                for strategy in qs:
-                    print(strategy)
-                    tch = TrendChecker.objects.filter(symbol=strategy.symbol_exchange,
-                                                      time_frame=strategy.time_frame).first()
 
-                    if tch.trade_long is True and strategy.long is True:
-                        if not Bot.objects.filter(strategy=strategy, user=user).exists():
-                            bot = Bot.objects.create(strategy=strategy, user=user)
-                            print("avvio bot con strategia long")
+            try:
 
-                            async_task("bot.services.runner.runnerbot", bot, Bot, BotLogger,
-                                       hook="bot.services.runner.get_runnerbot_hook")
+                qs = Strategy.objects.all()
+                usr = User.objects.all()
+                for user in usr:
+                    for strategy in qs:
+                        tch = TrendChecker.objects.filter(symbol=strategy.symbol_exchange,
+                                                          time_frame=strategy.time_frame).first()
 
-                    if tch.trade_short is True and strategy.short is True:
-                        if not Bot.objects.filter(strategy=strategy, user=user).exists():
-                            bot = Bot.objects.create(strategy=strategy, user=user)
-                            print("avvio bot con strategia short")
-                            async_task("bot.services.runner.runnerbot", bot, Bot, BotLogger,
-                                       hook="bot.services.runner.get_runnerbot_hook")
-            sleep(15)
+                        if tch.trade_long is True and strategy.long is True:
+                            if not Bot.objects.filter(strategy=strategy, user=user).exists():
+                                bot = Bot.objects.create(strategy=strategy, user=user)
+                                print("avvio bot con strategia long")
+
+                                async_task("bot.services.runner.runnerbot",
+                                           bot,
+                                           Bot,
+                                           BotLogger,
+                                           hook="bot.services.runner.get_runnerbot_hook", broker=broker)
+
+                        if tch.trade_short is True and strategy.short is True:
+                            if not Bot.objects.filter(strategy=strategy, user=user).exists():
+                                bot = Bot.objects.create(strategy=strategy, user=user)
+                                print("avvio bot con strategia short")
+
+                                async_task("bot.services.runner.runnerbot",
+                                           bot,
+                                           Bot,
+                                           BotLogger,
+                                           hook="bot.services.runner.get_runnerbot_hook", broker=broker)
+
+                sleep(15)
+
+            except Exception as e:
+                print(e)
+                Bot.objects.all().delete()
