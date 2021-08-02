5ef36787062281f9b7c09a87094b0edbfbecdd6b
egeominotti <egeominotti@gmail.com>
2021-08-02 03:34:04 +0200

    - removed binance api from env.dev

--- bot/management/commands/botdispatcher.py
+++ bot/management/commands/botdispatcher.py
@@ -13,14 +13,17 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         qs = Bot.objects.all()
         for bot in qs:
-            print(bot.strategy.symbol_exchange)
-            print(bot.strategy.time_frame)
-            tch = TrendChecker.objects.filter(symbol=bot.strategy.symbol_exchange, time_frame=bot.strategy.time_frame)
-            print(tch)
-            if bot.status != 'START':
-                async_task("bot.services.runner.runnerbot", bot, Bot, BotLogger)
+            tch = TrendChecker.objects.filter(symbol=bot.strategy.symbol_exchange,
+                                              time_frame=bot.strategy.time_frame).first()
+
+            if tch.trade_long is True and bot.status != 'START' and bot.long is True:
+                print("avvio bot con strategia long")
                 bot.status = 'START'
                 bot.save()
-            # Se l'andmento è long allora avvio bot long
+                async_task("bot.services.runner.runnerbot", bot, Bot, BotLogger)
 
-            # Se l'andamento è short allora avvio bot shot
+            if tch.trade_short is True and bot.status != 'START' and bot.short is True:
+                print("avvio bot con strategia short")
+                bot.status = 'START'
+                bot.save()
+                async_task("bot.services.runner.runnerbot", bot, Bot, BotLogger)
