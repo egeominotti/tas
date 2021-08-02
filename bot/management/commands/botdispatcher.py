1c3c11444645599b979146f909005800e29d8fcd
egeominotti <egeominotti@gmail.com>
2021-08-02 03:41:59 +0200

    - removed binance api from env.dev

--- bot/management/commands/botdispatcher.py
+++ bot/management/commands/botdispatcher.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from django.core.management import BaseCommand
 from django_q.tasks import async_task
 from analytics.models import TrendChecker
@@ -11,19 +13,24 @@ class Command(BaseCommand):
     help = 'DispatcherBot'
 
     def handle(self, *args, **kwargs):
-        qs = Bot.objects.all()
-        for bot in qs:
-            tch = TrendChecker.objects.filter(symbol=bot.strategy.symbol_exchange,
-                                              time_frame=bot.strategy.time_frame).first()
 
-            if tch.trade_long is True and bot.status != 'START' and bot.long is True:
-                print("avvio bot con strategia long")
-                bot.status = 'START'
-                bot.save()
-                async_task("bot.services.runner.runnerbot", bot, Bot, BotLogger)
+        while True:
 
-            if tch.trade_short is True and bot.status != 'START' and bot.short is True:
-                print("avvio bot con strategia short")
-                bot.status = 'START'
-                bot.save()
-                async_task("bot.services.runner.runnerbot", bot, Bot, BotLogger)
+            qs = Bot.objects.all()
+            for bot in qs:
+                tch = TrendChecker.objects.filter(symbol=bot.strategy.symbol_exchange,
+                                                  time_frame=bot.strategy.time_frame).first()
+
+                if tch.trade_long is True and bot.status != 'START' and bot.long is True:
+                    print("avvio bot con strategia long")
+                    bot.status = 'START'
+                    bot.save()
+                    async_task("bot.services.runner.runnerbot", bot, Bot, BotLogger)
+
+                if tch.trade_short is True and bot.status != 'START' and bot.short is True:
+                    print("avvio bot con strategia short")
+                    bot.status = 'START'
+                    bot.save()
+                    async_task("bot.services.runner.runnerbot", bot, Bot, BotLogger)
+
+            sleep(60)
