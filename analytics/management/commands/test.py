e5a74a581c84b3a17cd31e26451208ddcb6b736d
egeominotti <egeominotti@gmail.com>
2021-08-03 17:25:01 +0200

    - big fix

--- analytics/management/commands/test.py
+++ analytics/management/commands/test.py
@@ -1,18 +1,38 @@
 from exchange.model.binance import BinanceHelper
 from django.core.management import BaseCommand
 import logging
-
+from backtest.models import BackTest
+from backtest.model.backtest import Backtest as backtests
 logger = logging.getLogger('main')
 
-
-# twm = ThreadedWebsocketManager(api_key=config('API_KEY_BINANCE'), api_secret=config('API_SECRET_BINANCE'))
+from backtest.strategy.long.logic_function import *
+from backtest.strategy.short.logic_function import *
 
 
 class Command(BaseCommand):
     help = 'Prende gli indici delle candele a '
 
     def handle(self, *args, **kwargs):
-        bh = BinanceHelper(
-            symbol='BTCUSDT',
-        )
+
+        for instance in BackTest.objects.all():
+            bt = backtests(
+                instance=instance,
+                first_period=instance.start_period.strftime("%d %b,%Y"),
+                logic_entry=eval(instance.strategy.logic_entry.name),
+                logic_exit=eval(instance.strategy.logic_exit.name),
+                time_frame=instance.strategy.time_frame.time_frame,
+                symbol=instance.strategy.symbol_exchange.symbol,
+                take_profit_value=instance.strategy.logic_exit.take_profit,
+                stop_loss_value=instance.strategy.logic_exit.stop_loss,
+                ratio_value=instance.strategy.logic_entry.ratio,
+            )
+            return_value = bt.run()
+
+            item = {
+                'result': return_value,
+                'id': instance.id,
+                'symbol': instance.strategy.symbol_exchange.symbol,
+                'time_frame': instance.strategy.time_frame.time_frame
+            }
+            return item
 
