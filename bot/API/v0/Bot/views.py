4b78b2f7fcd99c2675aa0c67dd51265e63f7dbb9
egeominotti <egeominotti@gmail.com>
2021-08-06 01:30:42 +0200

    - init API

--- bot/API/v0/Bot/views.py
+++ bot/API/v0/Bot/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 from rest_framework.pagination import PageNumberPagination
 from bot.API.v0.Bot.serializers import BotSerializer
-from bot.models import Bot
+from bot.models import Bot, UserExchange, BotLogger
+from bot.model.bot import TradingBot
 
 
 class LargeResultsSetPagination(PageNumberPagination):
@@ -20,9 +21,23 @@ class BotList(generics.ListAPIView):
     serializer_class = BotSerializer
     queryset = Bot.objects.all().order_by('-created_at')
 
+
 class BotCreate(generics.CreateAPIView):
     serializer_class = BotSerializer
     queryset = Bot.objects.all().order_by('-created_at')
 
     def perform_create(self, serializer):
-        pass
+        bot = serializer.save()
+        usexg = UserExchange.objects.get(user=bot.user)
+        TradingBot(
+            current_bot=bot,
+            user=bot.user,
+            userexchange=usexg,
+            symbol=bot.coins.coins_taapi.symbol,
+            symbol_exchange=bot.coins.coins_exchange.symbol,
+            time_frame=bot.strategy.time_frame.time_frame,
+            func_entry=bot.strategy.logic_entry,
+            func_exit=bot.strategy.logic_exit,
+            logger=BotLogger,
+            bot_object=Bot
+        )
