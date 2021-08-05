57fb9d43f3a25f5d9b4c0713e1b57277c85eff10
egeominotti <egeominotti@gmail.com>
2021-08-05 18:03:52 +0200

    - pazzescoooo

--- deploy_update.sh
+++ deploy_update.sh
@@ -4,7 +4,9 @@ git pull
 pip3 install -r requirements.txt
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
-systemctl restart redis.service
-systemctl restart gunicorn.service
-systemctl restart nginx.service
+systemctl restart redis
+systemctl restart gunicorn
+systemctl restart qcluster
+systemctl restart dispatcherbot
+systemctl restart websocketstream
 source venv/bin/activate
