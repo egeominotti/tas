1b00f6f2d6aa4e4aeb267781a6c0c5d3ffb631a9
egeominotti <egeominotti@gmail.com>
2021-08-05 04:30:54 +0200

    - refactor

--- deploy_update.sh
+++ deploy_update.sh
@@ -4,7 +4,7 @@ git pull
 pip3 install -r requirements.txt
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
-systemctl restart redis
-systemctl restart gunicorn
-systemctl restart nginx
+systemctl restart redis.service
+systemctl restart gunicorn.service
+systemctl restart nginx.service
 source venv/bin/activate
