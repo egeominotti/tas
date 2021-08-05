38c2964c16ec5709a361c6174dbc2251a0e4e2f0
egeominotti <egeominotti@gmail.com>
2021-08-05 19:24:49 +0200

    - pazzescoooo

--- deploy_update.sh
+++ deploy_update.sh
@@ -4,7 +4,7 @@ git pull
 pip3 install -r requirements.txt
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
-systemctl restart redis
+systemctl restart mongod
 systemctl restart gunicorn
 systemctl restart qcluster
 systemctl restart dispatcherbot
