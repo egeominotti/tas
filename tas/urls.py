dd5d5dfc310288a1cab0ab1e2b51b77b8706794c
egeominotti <egeominotti@gmail.com>
2021-08-05 20:23:08 +0200

    - pazzescoooo

--- tas/urls.py
+++ tas/urls.py
@@ -2,15 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import path, include, re_path
 from django.urls import path
-
-
-def trigger_error(request):
-    division_by_zero = 1 / 0
+from tas.views import index
 
 
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('sentry-debug/', trigger_error),
     path('', include('analytics.urls')),
 ]
