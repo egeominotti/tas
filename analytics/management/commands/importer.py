a870a7764caa551a5e5a82cbe5ff634bf7c1b0d5
egeominotti <egeominotti@gmail.com>
2021-08-07 23:17:04 +0200

    - fix

--- analytics/management/commands/importer.py
+++ analytics/management/commands/importer.py
@@ -1,6 +1,5 @@
 import json
 from datetime import datetime
-from time import sleep
 from backtest.services.computedata import compute_data
 from binance import Client
 from decouple import config
@@ -10,11 +9,49 @@ from numpyencoder import NumpyEncoder
 from analytics.models import Importer
 import logging
 from strategy.models import TimeFrame, SymbolExchange
-
 from bot.services.telegram import Telegram
 
 logger = logging.getLogger('main')
 
+client = Client(config('API_KEY_BINANCE'), config('API_SECRET_BINANCE'))
+keyToRemove = ['timestamp', 'unix', 'open', 'high', 'low', 'close', 'volume']
+
+
+def save(symbol, time_frame):
+    print(symbol)
+    print(time_frame)
+
+    now = datetime.now().strftime("%d %b, %Y")
+    klines = client.get_historical_klines(symbol, time_frame, '17 Aug, 2017', now)
+
+    if len(klines) > 0:
+        klines_computed = compute_data(klines)
+
+        for item in klines_computed:
+
+            qs = Importer.objects.filter(Q(symbol=symbol) & Q(tf=time_frame) & Q(timestamp=item['timestamp']))
+            if len(qs) == 0:
+
+                imp = Importer.objects.create(
+                    symbol=symbol,
+                    tf=time_frame,
+                    unix=item['unix'],
+                    timestamp=item['timestamp'],
+                    open=item['open'],
+                    high=item['high'],
+                    low=item['low'],
+                    close=item['close'],
+                    volume=item['volume'],
+                )
+
+                for key in keyToRemove:
+                    del item[key]
+
+                Importer.objects \
+                    .filter(id=imp.id) \
+                    .update(
+                    indicators=json.dumps(item, cls=NumpyEncoder)
+                )
 
 class Command(BaseCommand):
     help = 'Salva tutti i dati di binance'
@@ -32,10 +69,7 @@ class Command(BaseCommand):
                     if s.to_import and t.to_import:
 
                         try:
-                            async_task("analytics.services.importer.save",
-                                       s.symbol,
-                                       t.time_frame,
-                                       hook="analytics.services.importer.get_save_hook")
+                            save(s.symbol, t.time_frame)
 
                         except Exception as e:
                             start = "Errore importazione dei dati: " + str(e) + " " + str(s.symbol) + " " + str(
