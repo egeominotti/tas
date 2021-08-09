3b2af1d6b8eff8416fdd87908b0faa7229081115
egeominotti <egeominotti@gmail.com>
2021-08-09 05:25:40 +0200

    - refactoring backtesting

--- backtest/management/commands/runnerbacktesting.py
+++ backtest/management/commands/runnerbacktesting.py
@@ -8,17 +8,19 @@ logger = logging.getLogger('main')
 
 
 def init():
-    for instance in BackTest.objects.filter(scheduled=False):
-        bt = BackTesting(
-            instance=instance,
-            start_period=instance.start_period,
-            end_period=instance.end_period,
-        )
+    qs = BackTest.objects.filter(scheduled=False)
+    if qs.count() > 0:
+        for instance in qs:
+            bt = BackTesting(
+                instance=instance,
+                start_period=instance.start_period,
+                end_period=instance.end_period,
+            )
 
-        thread = Thread(target=bt.run)
-        thread.daemon = True
-        thread.start()
-        thread.join()
+            thread = Thread(target=bt.run)
+            thread.daemon = True
+            thread.start()
+            thread.join()
 
 
 class Command(BaseCommand):
