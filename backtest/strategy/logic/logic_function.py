86fde496638c654279a06bc1cfdcb3a414e738f7
egeominotti <egeominotti@gmail.com>
2021-08-09 05:05:05 +0200

    - refactoring backtesting

--- backtest/strategy/logic/logic_function.py
+++ backtest/strategy/logic/logic_function.py
@@ -8,9 +8,7 @@ logger = logging.getLogger(__name__)
 def logicentry_first(item):
 
 
-    print(item)
     prev_item = find_prev_candle(item, 1)
-    print(prev_item)
     prev_indicators = json.loads(prev_item.indicators)
 
     if item['ema8'] > item['ema13'] > item['ema21'] > item['ema34']:
