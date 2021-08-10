cd8f1991ce98b468fd50cbdab3dde54a35968423
egeominotti <egeominotti@gmail.com>
2021-08-10 23:58:51 +0200

    - reset

--- tas/asgi.py
+++ tas/asgi.py
@@ -1,13 +1,14 @@
+from django.core.asgi import get_asgi_application
 import os
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-import bot.routing
+
+
+django_asgi_app = get_asgi_application()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tas.settings")
 
-django_asgi_app = get_asgi_application()
+import bot.routing
 
 application = ProtocolTypeRouter({
   "http": django_asgi_app,
