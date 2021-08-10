382b53e196eb15b821b20df7095bf6a1be2ee7ef
egeominotti <egeominotti@gmail.com>
2021-08-10 07:20:45 +0200

    - reset

--- deploy_update.sh
+++ deploy_update.sh
@@ -5,7 +5,5 @@ pip3 install -r requirements.txt
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
 systemctl restart gunicorn
-systemctl restart balanceuserupdate
-systemctl restart websocketstream
 systemctl restart runnerbacktesting
 source venv/bin/activate
