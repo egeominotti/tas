f4a2eea45dfeb8b40dfb4111da5bb8be65d11e76
egeominotti <egeominotti@gmail.com>
2021-08-10 23:47:30 +0200

    - reset

--- bot/routing.py
+++ bot/routing.py
@@ -1,7 +1,6 @@
 from django.urls import re_path
-
-from . import consumers
+from bot.consumers import BotConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r"^ws/bot/list$", BotConsumer.as_asgi()),
 ]
--- tas/asgi.py
+++ tas/asgi.py
@@ -1,33 +1,18 @@
-# import os
-# from bot.consumers import BotConsumer
-# from channels.auth import AuthMiddlewareStack
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# from django.conf.urls import url
-# from django.core.asgi import get_asgi_application
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tas.settings')
-#
-# django_asgi_app = get_asgi_application()
-#
-#
-#
-# application = ProtocolTypeRouter({
-#     # Django's ASGI application to handle traditional HTTP requests
-#     "http": django_asgi_app,
-#
-#     # WebSocket chat handler
-#     "websocket": AuthMiddlewareStack(
-#         URLRouter([
-#             url(r"^ws/observer/bot/$", BotConsumer.as_asgi()),
-#         ])
-#     ),
-# })
-
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import bot.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tas.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tas.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            bot.routing.websocket_urlpatterns
+        )
+    ),
+})
 
