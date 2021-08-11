386ce5327f71f4c82c42fa4fd4ff9e12159f6b54
egeominotti <egeominotti@gmail.com>
2021-08-11 16:20:37 +0200

    - reset

--- deploy_update.sh
+++ deploy_update.sh
@@ -4,7 +4,7 @@ git pull
 pip3 install -r requirements.txt
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
-systemctl restart gunicorn
+systemctl restart daphne
 systemctl restart nginx
 systemctl restart runnerbacktesting
 source venv/bin/activate
