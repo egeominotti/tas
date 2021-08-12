ae768e4e9ad8374cdfad720047b89649ec1e864f
egeominotti <egeominotti@gmail.com>
2021-08-12 15:46:08 +0200

    - removed buffer

--- bot/strategy/logic/logic_function.py
+++ bot/strategy/logic/logic_function.py
@@ -90,7 +90,7 @@ def logicentry_bot_rsi_20_bollinger(item):
             item['entry_candle'] = item['candle_close']
             return True
 
-        redis.set(key, {'candle_is_closed': False})
+        redis.set(key, json.dumps({'candle_is_closed': False}))
         return False
 
 
