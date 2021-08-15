cfd441afb0aa10f246231620f4fea7a4ba988a74
egeominotti <egeominotti@gmail.com>
2021-08-15 17:46:38 +0200

    - fix quantity

--- bot/admin.py
+++ bot/admin.py
@@ -9,7 +9,7 @@ class BotAdmin(admin.ModelAdmin):
     ordering = ('id',)
     list_display = (
         'name', 'user', 'running', 'abort', 'strategy', 'coins', 'created_at', 'updated_at',)
-    readonly_fields = ('name', 'abort')
+    readonly_fields = ('name', 'running', 'abort')
     exclude = ['flgEnable', ]
 
     def has_delete_permission(self, request, obj=None):
