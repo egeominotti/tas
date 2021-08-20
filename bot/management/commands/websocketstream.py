e3619e385052d426a20138575cfc526e80fe6451
egeominotti <egeominotti@gmail.com>
2021-08-20 06:25:07 +0200

    - fix

--- bot/management/commands/websocketstream.py
+++ bot/management/commands/websocketstream.py
@@ -53,23 +53,25 @@ class Command(BaseCommand):
 
                                 qs = BufferRecordData.objects.filter(key=key)
 
-                                if qs.count() <= 364:
-
-                                    BufferRecordData.objects.create(
-                                        key=key,
-                                        symbol=SymbolExchange.objects.get(symbol=v.get('symbol')).symbol,
-                                        time_frame=v.get('interval'),
-                                        open_candle=float(v.get('open_price')),
-                                        close_candle=float(v.get('close_price')),
-                                        high_candle=float(v.get('high_price')),
-                                        low_candle=float(v.get('low_price')),
-                                        is_closed=v.get('is_closed'),
-                                        unix=v.get('kline_start_time'),
-                                        volume=v.get('base_volume')
-                                    )
-
-                                else:
+                                if qs.count() >= 364:
                                     qs.first().delete()
+
+                                if qs.count() <= 364:
+                                    BufferRecordData.objects.create(
+                                            key=key,
+                                            symbol=SymbolExchange.objects.get(symbol=v.get('symbol')).symbol,
+                                            time_frame=v.get('interval'),
+                                            open_candle=float(v.get('open_price')),
+                                            close_candle=float(v.get('close_price')),
+                                            high_candle=float(v.get('high_price')),
+                                            low_candle=float(v.get('low_price')),
+                                            is_closed=v.get('is_closed'),
+                                            unix=v.get('kline_start_time'),
+                                            volume=v.get('base_volume')
+                                        )
+
+
+
 
 
 
