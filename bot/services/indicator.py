786a20743a4044a112f134bb65933e69601bd618
egeominotti <egeominotti@gmail.com>
2021-08-21 17:44:53 +0200

    - fix

--- bot/services/indicator.py
+++ bot/services/indicator.py
@@ -20,12 +20,11 @@ class RealTimeIndicator:
     def compute(self):
 
         try:
-            sleep(1)
-            klines = self.client.get_klines(symbol=self.symbol, interval=self.time_frame, limit=400)
+            klines = self.client.get_klines(symbol=self.symbol, interval=self.time_frame, limit=500)
         except Exception as e:
             print("Binance Error:" + str(e))
             sleep(5)
-            klines = self.client.get_klines(symbol=self.symbol, interval=self.time_frame, limit=400)
+            klines = self.client.get_klines(symbol=self.symbol, interval=self.time_frame, limit=500)
 
         open = [float(entry[1]) for entry in klines]
         high = [float(entry[2]) for entry in klines]
