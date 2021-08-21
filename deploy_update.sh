0e0d262ab6bb2701578d88e0bac8690766c92bc4
egeominotti <egeominotti@gmail.com>
2021-08-21 04:43:36 +0200

    - fix

--- deploy_update.sh
+++ deploy_update.sh
@@ -2,8 +2,8 @@
 source venv/bin/activate
 git pull
 pip3 install -r requirements.txt
-python3 manage.py migrate --noinput
-python3 manage.py collectstatic --noinput
+python3.9 manage.py migrate --noinput
+python3.9 manage.py collectstatic --noinput
 systemctl restart daphne
 systemctl restart nginx
 systemctl restart runnerbacktesting
