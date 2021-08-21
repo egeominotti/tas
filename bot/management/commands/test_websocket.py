a9bddc3a4bfb6d64e806697d96ec55aea8678ed8
egeominotti <egeominotti@gmail.com>
2021-08-21 05:50:26 +0200

    - fix

--- bot/management/commands/test_websocket.py
+++ bot/management/commands/test_websocket.py
@@ -9,7 +9,17 @@ class Command(BaseCommand):
     help = 'Test'
 
     def handle(self, *args, **kwargs):
+
+        print("1 MINUTO")
         indicator = Indicator('RVNUSDT','1m')
+        print(indicator.ema(5))
+        print(indicator.rsi(14))
+        print(indicator.bbands(20))
+        print(indicator.candle())
+
+
+        print("5 MINUTI")
+        indicator = Indicator('RVNUSDT','5m')
         print(indicator.ema(5))
         print(indicator.rsi(14))
         print(indicator.bbands(20))
