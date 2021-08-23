8ec838d31f3f2d11454b5fbac21959089583931f
egeominotti <egeominotti@gmail.com>
2021-08-23 22:54:17 +0200

    - spot / futures

--- bot/management/commands/collectdata.py
+++ bot/management/commands/collectdata.py
@@ -13,28 +13,11 @@ class Command(BaseCommand):
     help = 'Valori real time delle candele'
 
     def handle(self, *args, **kwargs):
-        r = redis.Redis(host=decouple.config('REDIS_HOST'), port=6379, db=0)
-
-        times = ['1m', '5m', '15m', '30m', '1h', '2h', '4h', '6h', '8h', '12h', '1d', '3d', '1w', '1M']
-
-        symbolList = []
-        for symbol in SymbolExchange.objects.all():
-            symbolList.append(symbol.symbol.lower())
-
-        for symbol in symbolList:
-            for interval in times:
-                key = str(SymbolExchange.objects.get(symbol=symbol)) + "_" + str(interval) + "_CLOSED"
-                if r.exists(key):
-                    val = r.get(key)
-                    print(val)
-                    print(val)
-                    for k in val:
-                        print(k['candle_close'])
 
 
-        # indicator = RealTimeIndicator('RVNUSDT', '1m','vyghMLzH2Pvr0TCoV11Equ9kIK2jxL6ZpDh8pyUBz4hvAWXSLWO6rBHbogQmX9lH','yTmr8uu0w3ARIzTlYadGkWX79BlTHSybzzJeInrWcjUoygP3K7t81j4WXd8amMOM')
-        # indicator.compute()
-        # print(indicator.ema(5))
-        # print(indicator.rsi(14))
-        # print(indicator.bbands(20))
-        # print(indicator.candle())
+        indicator = RealTimeIndicator('RVNUSDT', '1m','vyghMLzH2Pvr0TCoV11Equ9kIK2jxL6ZpDh8pyUBz4hvAWXSLWO6rBHbogQmX9lH','yTmr8uu0w3ARIzTlYadGkWX79BlTHSybzzJeInrWcjUoygP3K7t81j4WXd8amMOM')
+        indicator.compute()
+        print(indicator.ema(5))
+        print(indicator.rsi(14))
+        print(indicator.bbands(20))
+        print(indicator.candle())
--- bot/services/indicator.py
+++ bot/services/indicator.py
@@ -1,7 +1,13 @@
 from time import sleep
+
+import decouple
+import json
+import redis
 from binance import Client
 import talib
 import numpy as np
+
+from strategy.models import SymbolExchange
 
 
 class RealTimeIndicator:
@@ -13,7 +19,9 @@ class RealTimeIndicator:
     def __init__(self, symbol, time_frame, api_key, api_secret):
         self.symbol = symbol
         self.time_frame = time_frame
+        self.redis = False
         self.client = Client(api_key, api_secret)
+        self.redis_client = redis.Redis(host=decouple.config('REDIS_HOST'), port=6379, db=0)
 
     def compute(self, start_time=None):
 
@@ -38,31 +46,62 @@ class RealTimeIndicator:
         ]
         """
 
-        try:
-            if start_time is not None:
-                klines = self.client.get_klines(symbol=self.symbol, interval=self.time_frame, endTime=start_time)
-            if start_time is None:
-                klines = self.client.get_klines(symbol=self.symbol, interval=self.time_frame)
+        if self.redis:
+            try:
+                if start_time is not None:
+                    klines = self.client.get_klines(symbol=self.symbol, interval=self.time_frame, endTime=start_time)
+                if start_time is None:
+                    klines = self.client.get_klines(symbol=self.symbol, interval=self.time_frame)
 
-        except Exception as e:
-            print("Binance Error:" + str(e))
-            sleep(30)
-            if start_time is not None:
-                klines = self.client.get_klines(symbol=self.symbol, interval=self.time_frame, endTime=start_time)
-            if start_time is None:
-                klines = self.client.get_klines(symbol=self.symbol, interval=self.time_frame)
+            except Exception as e:
+                print("Binance Error:" + str(e))
+                sleep(30)
+                if start_time is not None:
+                    klines = self.client.get_klines(symbol=self.symbol, interval=self.time_frame, endTime=start_time)
+                if start_time is None:
+                    klines = self.client.get_klines(symbol=self.symbol, interval=self.time_frame)
 
-        open_time =     [entry[0] for entry in klines]
-        open =          [float(entry[1]) for entry in klines]
-        high =          [float(entry[2]) for entry in klines]
-        low =           [float(entry[3]) for entry in klines]
-        close =         [float(entry[4]) for entry in klines]
-        close_time =    [entry[6] for entry in klines]
+            open_time =     [entry[0] for entry in klines]
+            open =          [float(entry[1]) for entry in klines]
+            high =          [float(entry[2]) for entry in klines]
+            low =           [float(entry[3]) for entry in klines]
+            close =         [float(entry[4]) for entry in klines]
+            close_time =    [entry[6] for entry in klines]
 
-        self.close_array = np.asarray(close)
-        self.open_array = np.asarray(open)
-        self.low_array = np.asarray(low)
-        self.high_array = np.asarray(high)
+            self.close_array = np.asarray(close)
+            self.open_array = np.asarray(open)
+            self.low_array = np.asarray(low)
+            self.high_array = np.asarray(high)
+
+        else:
+
+            key = str(SymbolExchange.objects.get(symbol=self.symbol.upper())) + "_" + str(self.time_frame) + "_REALTIME"
+            if self.redis_client.exists(key):
+
+                """
+                 candle_realtime = {
+
+                                    'candle_close': close_price,
+                                    'candle_open': open_price,
+                                    'candle_high': high_price,
+                                    'candle_low': low_price,
+                                    'is_closed': is_closed,
+                                    'time_milliseconds': kline_start_time,
+                                    # 'time_datetime': datetime.datetime.fromtimestamp(kline['kline_start_time'] / 1000.0,tz=datetime.timezone.utc),
+                                }
+                """
+
+                klines = json.loads(self.redis_client.get(key))
+
+                open = [float(entry['candle_open']) for entry in klines]
+                high = [float(entry['candle_high']) for entry in klines]
+                low = [float(entry['candle_low']) for entry in klines]
+                close = [float(entry['candle_low']) for entry in klines]
+
+                self.close_array = np.asarray(close)
+                self.open_array = np.asarray(open)
+                self.low_array = np.asarray(low)
+                self.high_array = np.asarray(high)
 
     def candle(self, backtrack=-1):
 
