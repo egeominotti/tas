c008bf4633d6fcf83334c36c24ac51d4f7f4ce53
egeominotti <egeominotti@gmail.com>
2021-08-23 20:35:46 +0200

    - spot / futures

--- bot/management/commands/websocketstream.py
+++ bot/management/commands/websocketstream.py
@@ -1,3 +1,5 @@
+import datetime
+
 import decouple
 from django.core.management import BaseCommand
 import logging
@@ -29,7 +31,6 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
 
         r = redis.Redis(host=decouple.config('REDIS_HOST'), port=6379, db=0)
-        # https://docs.python.org/3/library/logging.html#logging-levels
         logging.basicConfig(level=logging.ERROR,
                             filename=os.path.basename(__file__) + '.log',
                             format="{asctime} [{levelname:8}] {process} {thread} {module}: {message}",
@@ -39,19 +40,21 @@ class Command(BaseCommand):
         for symbol in SymbolExchange.objects.all():
             symbolList.append(symbol.symbol.lower())
 
-        # create instance of BinanceWebSocketApiManager
         binance_websocket_api_manager = BinanceWebSocketApiManager(exchange="binance.com", output_default="UnicornFy")
 
         binance_websocket_api_manager.create_stream('kline_1m', symbolList, stream_label="kline_1m", output="UnicornFy")
         binance_websocket_api_manager.create_stream('kline_5m', symbolList, stream_label="kline_5m", output="UnicornFy")
-        binance_websocket_api_manager.create_stream('kline_15m', symbolList, stream_label="kline_5m", output="UnicornFy")
-        binance_websocket_api_manager.create_stream('kline_30m', symbolList, stream_label="kline_30m", output="UnicornFy")
+        binance_websocket_api_manager.create_stream('kline_15m', symbolList, stream_label="kline_5m",
+                                                    output="UnicornFy")
+        binance_websocket_api_manager.create_stream('kline_30m', symbolList, stream_label="kline_30m",
+                                                    output="UnicornFy")
         binance_websocket_api_manager.create_stream('kline_1h', symbolList, stream_label="kline_1h", output="UnicornFy")
         binance_websocket_api_manager.create_stream('kline_2h', symbolList, stream_label="kline_2h", output="UnicornFy")
         binance_websocket_api_manager.create_stream('kline_4h', symbolList, stream_label="kline_4h", output="UnicornFy")
         binance_websocket_api_manager.create_stream('kline_6h', symbolList, stream_label="kline_6h", output="UnicornFy")
         binance_websocket_api_manager.create_stream('kline_8h', symbolList, stream_label="kline_8h", output="UnicornFy")
-        binance_websocket_api_manager.create_stream('kline_12h', symbolList, stream_label="kline_12h", output="UnicornFy")
+        binance_websocket_api_manager.create_stream('kline_12h', symbolList, stream_label="kline_12h",
+                                                    output="UnicornFy")
         binance_websocket_api_manager.create_stream('kline_1d', symbolList, stream_label="kline_1d", output="UnicornFy")
         binance_websocket_api_manager.create_stream('kline_3d', symbolList, stream_label="kline_3d", output="UnicornFy")
         binance_websocket_api_manager.create_stream('kline_1w', symbolList, stream_label="kline_1w", output="UnicornFy")
@@ -66,79 +69,42 @@ class Command(BaseCommand):
             else:
                 if oldest_stream_data_from_stream_buffer is not None:
                     try:
-                        if oldest_stream_data_from_stream_buffer['event_time'] >= oldest_stream_data_from_stream_buffer['kline']['kline_close_time']:
-                            print(oldest_stream_data_from_stream_buffer['kline']['symbol'])
-                            print(oldest_stream_data_from_stream_buffer['kline']['interval'])
-                            print(oldest_stream_data_from_stream_buffer['kline']['open_price'])
-                            print(oldest_stream_data_from_stream_buffer['kline']['close_price'])
-                            print(oldest_stream_data_from_stream_buffer['kline']['high_price'])
-                            print(oldest_stream_data_from_stream_buffer['kline']['low_price'])
-                            print(oldest_stream_data_from_stream_buffer['kline']['is_closed'])
-                            # print only the last kline
-                            #print(f"UnicornFy: {oldest_stream_data_from_stream_buffer}")
+                        # if candle is closed save record
+                        if oldest_stream_data_from_stream_buffer['event_time'] >=\
+                                oldest_stream_data_from_stream_buffer['kline']['kline_close_time']:
+                            if oldest_stream_data_from_stream_buffer['kline']['is_closed']:
+
+                                kline = oldest_stream_data_from_stream_buffer['kline']
+                                symbol = kline['symbol']
+                                interval = kline['interval']
+                                open_price = kline['open_price']
+                                close_price = kline['close_price']
+                                high_price = kline['high_price']
+                                low_price = kline['low_price']
+                                is_closed = kline['is_closed']
+                                kline_start_time = kline['kline_start_time']
+
+                                key = str(SymbolExchange.objects.get(symbol=symbol)) + "_" + str(interval)
+                                print(key)
+                                candle_realtime = {
+
+                                    'candle_close': close_price,
+                                    'candle_open': open_price,
+                                    'candle_high': high_price,
+                                    'candle_low': low_price,
+                                    'is_closed': is_closed,
+                                    'time_milliseconds': kline_start_time,
+                                    'time_datetime': datetime.datetime.fromtimestamp(kline['kline_start_time'] / 1000.0,
+                                                                                     tz=datetime.timezone.utc),
+                                }
+                                print(candle_realtime)
+                                r.set(key, json.dumps(candle_realtime))
+
                     except KeyError:
                         pass
-                        #print(f"dict: {oldest_stream_data_from_stream_buffer}")
                     except TypeError:
                         pass
-                        #print(f"raw_data: {oldest_stream_data_from_stream_buffer}")
-        # r = redis.Redis(host=decouple.config('REDIS_HOST'), port=6379, db=0)
-        #
-        # symbolList = []
-        # for symbol in SymbolExchange.objects.all():
-        #     symbolList.append(symbol.symbol.lower())
-        #
-        # # klines = ['kline_1m', 'kline_5m', 'kline_15m', 'kline_30m', 'kline_1h', 'kline_2h', 'kline_4h', 'kline_6h',
-        # #           'kline_8h', 'kline_12h', 'kline_1d', 'kline_3d', 'kline_1w', 'kline_1M']
-        #
-        # klines = ['kline_1m',
-        #           'kline_5m',
-        #           'kline_15m',
-        #           'kline_30m',
-        #           'kline_1h',
-        #           'kline_4h',
-        #           'kline_1d',
-        #           'kline_1w',
-        #           'kline_1M']
-        #
-        # binance_websocket_api_manager = BinanceWebSocketApiManager(exchange="binance.com")
-        # binance_websocket_api_manager.create_stream(klines, symbolList, output="UnicornFy")
-        #
-        # def save_data(v):
-        #
-        #     candle_is_closed = v.get('is_closed')
-        #     symbol = v.get('symbol')
-        #     interval = v.get('interval')
-        #
-        #     # Candle not closed - save to redis real time data
-        #     key = str(SymbolExchange.objects.get(symbol=symbol)) + "_" + str(interval)
-        #     candle_realtime = {
-        #         'candle_close': float(v.get('close_price')),
-        #         'candle_open': float(v.get('open_price')),
-        #         'candle_high': float(v.get('high_price')),
-        #         'candle_low': float(v.get('low_price')),
-        #         'candle_is_closed': v.get('is_closed'),
-        #         'time': v.get('kline_start_time')
-        #     }
-        #     print(candle_realtime)
-        #     r.set(key, json.dumps(candle_realtime))
-        #
-        #     # candle_is_closed = v.get('is_closed')
-        #     # symbol = v.get('symbol')
-        #     # interval = v.get('interval')
-        #     #
-        #     # # Candle not closed - save to redis real time data
-        #     # key = str(SymbolExchange.objects.get(symbol=symbol)) + "_" + str(interval)
-        #     # candle_realtime = {
-        #     #     'candle_close': float(v.get('close_price')),
-        #     #     'candle_open': float(v.get('open_price')),
-        #     #     'candle_high': float(v.get('high_price')),
-        #     #     'candle_low': float(v.get('low_price')),
-        #     #     'candle_is_closed': v.get('is_closed'),
-        #     #     'time': v.get('kline_start_time')
-        #     # }
-        #     # r.set(key, json.dumps(candle_realtime))
-        #
+
         #     # Candle closed - save to db
         #     # if candle_is_closed:
         #     #
