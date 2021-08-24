bd71d8d6290350570c0347ad67b87b1c1fcc3105
egeominotti <egeominotti@gmail.com>
2021-08-24 03:22:26 +0200

    - spot / futures

--- bot/management/commands/runnerbot.py
+++ bot/management/commands/runnerbot.py
@@ -8,12 +8,10 @@ from bot.models import Bot, UserExchange, BotLogger
 from queue import Queue
 from collections import deque
 
-
 logger = logging.getLogger('main')
 
 
 def spawnbot(instance) -> None:
-
     tb = TradingBot(
         instance=instance,
         userexchange=UserExchange.objects.get(user=instance.user),
@@ -27,17 +25,17 @@ def init() -> None:
 
     while True:
 
-        if datetime.datetime.now().second == 15:
-            qs = Bot.objects.filter(running=False, abort=False)
-            if qs.count() > 0:
-                for instance in qs:
-                    thread = Thread(target=spawnbot, name=instance.name, args=(instance,))
-                    thread.daemon = True
-                    instance.running = True
-                    instance.save()
-                    thread.start()
-                    print("Sart thread: " + str(thread))
-
+        # if datetime.datetime.now().second == 15:
+        qs = Bot.objects.filter(running=False, abort=False)
+        if qs.count() > 0:
+            for instance in qs:
+                thread = Thread(target=spawnbot, name=instance.name, args=(instance,))
+                thread.daemon = True
+                instance.running = True
+                instance.save()
+                thread.start()
+                print("Sart thread: " + str(thread))
+        sleep(1)
 
 class Command(BaseCommand):
     help = 'AsyncRunnerBot'
