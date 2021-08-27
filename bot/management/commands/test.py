780ad4af9291e6f362f0631e8be40d6768e6be59
egeominotti <egeominotti@gmail.com>
2021-08-27 20:12:48 +0200

    - spot feature support

--- bot/management/commands/test.py
+++ bot/management/commands/test.py
@@ -24,8 +24,8 @@ class Command(BaseCommand):
     help = 'AsyncRunnerBot'
 
     def handle(self, *args, **kwargs):
-        client = Client()
 
+        client = Client()
 
         # del close[0]
         # LEN = len(close)
@@ -37,46 +37,46 @@ class Command(BaseCommand):
         close_array = None
         arr2 = None
         while True:
-            if close_array is None:
-                klines = client \
-                    .futures_klines(symbol='RVNUSDT',
-                                    interval='1m',
-                                    limit=250
-                                    )
-                del klines[-1]
-                close = [double(entry[4]) for entry in klines]
-                close_array = np.asarray(close)
 
-                if len(close_array) >= 14 and close_array is not None:
-                    rsi = talib.RSI(close_array, timeperiod=14)
-                    print(rsi[-1])
+            klines = client \
+                .futures_klines(symbol='RVNUSDT',
+                                interval='1m',
+                                limit=250
+                                )
+            del klines[-1]
+            print(klines)
+            close = [double(entry[4]) for entry in klines]
+            close_array = np.asarray(close)
 
-            else:
-                symbol = 'RVNUSDT'
-                timeframe = '1m'
-                key = symbol + "_" + str(timeframe) + "_FUTURES"
-                data = json.loads(r.get(key))
-                #close = [double(entry['candle_close']) for entry in data]
-                if data.get('is_closed') is True:
-                    arr2 = np.array([double(data['candle_close'])])
-                    print(arr2)
-                    print("iNSEISCO NUOVO VALORE")
-                    r.set(key, json.dumps({'is_closed': False}))
-
-            if arr2 is not None:
-                arr_flat = np.append(close_array, arr2)
-                print(arr_flat)
-
-                if len(arr_flat) >= 14 and arr_flat is not None:
-                    rsi = talib.RSI(arr_flat, timeperiod=14)
-                    print(rsi[-1])
-
+            if len(close_array) >= 14 and close_array is not None:
+                rsi = talib.RSI(close_array, timeperiod=14)
+                print(rsi[-1])
             sleep(1)
-                #
-                # arr_flat = np.append(close_array, arr2)
-                # print(arr_flat)
-                # if len(arr_flat) >= 14 and arr_flat is not None:
-                #     rsi = talib.RSI(arr_flat, timeperiod=14)
-                #     print(rsi[-1])
-                # sleep(1)
-
+        # else:
+        #     symbol = 'RVNUSDT'
+        #     timeframe = '1m'
+        #     key = symbol + "_" + str(timeframe) + "_FUTURES"
+        #     data = json.loads(r.get(key))
+        #     #close = [double(entry['candle_close']) for entry in data]
+        #     if data.get('is_closed') is True:
+        #         arr2 = np.array([double(data['candle_close'])])
+        #         print(arr2)
+        #         print("iNSEISCO NUOVO VALORE")
+        #         r.set(key, json.dumps({'is_closed': False}))
+        #
+        # if arr2 is not None:
+        #     arr_flat = np.append(close_array, arr2)
+        #     print(arr_flat)
+        #
+        #     if len(arr_flat) >= 14 and arr_flat is not None:
+        #         rsi = talib.RSI(arr_flat, timeperiod=14)
+        #         print(rsi[-1])
+        #
+        # sleep(1)
+        #
+        # arr_flat = np.append(close_array, arr2)
+        # print(arr_flat)
+        # if len(arr_flat) >= 14 and arr_flat is not None:
+        #     rsi = talib.RSI(arr_flat, timeperiod=14)
+        #     print(rsi[-1])
+        # sleep(1)
