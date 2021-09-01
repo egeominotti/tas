804a828ca7c40d7c3353586b194160ab279e1df0
egeominotti <egeominotti@gmail.com>
2021-09-01 19:03:20 +0200

    - init cluster bot

--- bot/strategy/logic/logic_function.py
+++ bot/strategy/logic/logic_function.py
@@ -18,6 +18,7 @@ def logicentry_bot_rsi_20_bollinger(item: dict) -> None:
     valueLowerBand = bbands.get('valueLowerBand')
     valueUpperBand = bbands.get('valueUpperBand')
 
+    # Long condition
     if rsi < 20 and item['candle_close'] <= valueLowerBand:
         item['type'] = 0  # type = 0 corrisponde ad una entrata long
         item['entry'] = True
