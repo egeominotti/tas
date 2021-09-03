982332af3ce2b015aee9a2250f266ac2c31a0670
egeominotti <egeominotti@gmail.com>
2021-09-03 14:58:06 +0200

    - api

--- bot/management/commands/test.py
+++ bot/management/commands/test.py
@@ -15,37 +15,24 @@ logger = logging.getLogger('main')
 # each ohlcv candle is a list of [ timestamp, open, high, low, close, volume ]
 index = 4  # use close price from each ohlcv candle
 
-height = 15
-length = 80
 r = redis.Redis(host=decouple.config('REDIS_HOST'), port=6379, db=0)
-
+KEY = 'FUTURES'
 
 class Command(BaseCommand):
     help = 'AsyncRunnerBot'
 
     def handle(self, *args, **kwargs):
-        client = Client('vyghMLzH2Pvr0TCoV11Equ9kIK2jxL6ZpDh8pyUBz4hvAWXSLWO6rBHbogQmX9lH',
-                        'yTmr8uu0w3ARIzTlYadGkWX79BlTHSybzzJeInrWcjUoygP3K7t81j4WXd8amMOM')
 
-        client.futures_change_leverage(symbol='ETHUSDT', marginType='ISOLATED', leverage=2)
-        order = client.futures_create_order(
-            symbol='ETHUSDT',
-            side='SELL',
-            type='MARKET',
-            quantity=0.02,
-        )
-        val = client.futures_get_order(symbol=order.get('symbol'), orderId=order.get('orderId'))
-        print(val.get('avgPrice'))
-        # info_spot = client.get_exchange_info()
-        # info_futures = client.futures_exchange_info()
-        #
-        # coinsspot = []
-        # for coins_futures in info_spot['symbols']:
-        #     coinsspot.append(coins_futures['symbol'])
-        # print(len(coinsspot))
-        #
-        # coinsfutures = []
-        # for coins_futures in info_futures['symbols']:
-        #     print(coins_futures['pricePrecision'])
-        #     coinsfutures.append(coins_futures['symbol'])
-        # print(len(coinsfutures))
+        symbol = 'RVNUSDT'
+        timeframe = '5m'
+        key = symbol + "_" + timeframe + "_" + KEY + "_" + "_REALTIME"
+        while True:
+            data = r.smembers(key)
+            for k in data:
+                print(k[0])
+        # print(data)
+        # close = [double(entry[4]) for entry in data]
+        # close_array = np.asarray(close)
+        # if len(close_array) > 14 and close_array is not None:
+        #     rsi = talib.RSI(close_array, timeperiod=14)
+        #     print(rsi[-1])
