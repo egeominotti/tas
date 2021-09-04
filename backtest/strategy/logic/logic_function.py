b1774e62ec0d41667012a463023c3c3b76bc2d09
egeominotti <egeominotti@gmail.com>
2021-09-04 13:43:38 +0200

    - api

--- backtest/strategy/logic/logic_function.py
+++ backtest/strategy/logic/logic_function.py
@@ -1,8 +1,5 @@
-import json
-import logging
 from backtest.services.util import find_prev_candle
 
-logger = logging.getLogger(__name__)
 
 """
 STRATEGY 2: RSI 20 BOLLINGER
@@ -32,30 +29,24 @@ def logicexit_long_backtest_rsi_20_bollinger(item):
     return False
 
 
-def logicentry_short_backtest_rsi_20_bollinger(item):
-    rsi = item['rsi']
-    upperband = item['upperband']
-    if rsi > 80 and item['close'] >= upperband:
-        return True
-
-    return False
-
-
-def logicexit_short_backtest_rsi_20_bollinger(item):
-    lowerband = item['lowerband']
-
-    if item['close'] <= lowerband:
-        item['takeprofit_func'] = True
-        return True
-
-    if item['close'] <= item['entry'] * item['stoploss']:
-        item['stoploss_func'] = True
-        return True
-
-    return False
-
-"""
-STRATEGY 3: MACD
-"""
-
-
+# def logicentry_short_backtest_rsi_20_bollinger(item):
+#     rsi = item['rsi']
+#     upperband = item['upperband']
+#     if rsi > 80 and item['close'] >= upperband:
+#         return True
+#
+#     return False
+#
+#
+# def logicexit_short_backtest_rsi_20_bollinger(item):
+#     lowerband = item['lowerband']
+#
+#     if item['close'] <= lowerband:
+#         item['takeprofit_func'] = True
+#         return True
+#
+#     if item['close'] <= item['entry'] * item['stoploss']:
+#         item['stoploss_func'] = True
+#         return True
+#
+#     return False
