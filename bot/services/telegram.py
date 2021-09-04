c855e7d8e5bfd1435a4a3c9eed56582a73c9808e
egeominotti <egeominotti@gmail.com>
2021-09-04 19:17:00 +0200

    - api

--- bot/services/telegram.py
+++ bot/services/telegram.py
@@ -12,15 +12,18 @@ class Telegram:
 
     # async send
     def async_send(self, text):
+
         try:
 
             send_text = 'https://api.telegram.org/bot' + self.bot_token + '/sendMessage?chat_id=' + self.bot_chat_id + '&parse_mode=Markdown&text=' + text
             response = requests.get(send_text)
-            return response.json()
+            #return response.json()
+            sys.exit()
 
         except Exception as e:
             exception = 'Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e
             print(exception)
+
 
         sys.exit()
 
