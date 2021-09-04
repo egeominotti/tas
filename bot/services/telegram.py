d93e6bc27cdb76e75d8523ac84dcd69df1a44a32
egeominotti <egeominotti@gmail.com>
2021-09-04 19:17:13 +0200

    - api

--- bot/services/telegram.py
+++ bot/services/telegram.py
@@ -16,7 +16,7 @@ class Telegram:
         try:
 
             send_text = 'https://api.telegram.org/bot' + self.bot_token + '/sendMessage?chat_id=' + self.bot_chat_id + '&parse_mode=Markdown&text=' + text
-            response = requests.get(send_text)
+            requests.get(send_text)
             #return response.json()
             sys.exit()
 
