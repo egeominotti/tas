46b3be8c41c4c4157f3de2ff020ad33f42570114
kernel_void <egeominotti@gmail.com>
2021-09-07 00:03:27 +0200

    - fix

--- bot/management/commands/indicatorcalculator.py
+++ bot/management/commands/indicatorcalculator.py
@@ -1,5 +1,5 @@
 from time import sleep
-
+import datetime
 import decouple
 import json
 import redis
@@ -31,6 +31,7 @@ class Command(BaseCommand):
                 if r.exists(key):
                     klines = json.loads(r.get(key))
                     if len(klines) > 0:
+
                         computed_data = compute_data_to_store(klines)
                         val = json.loads(computed_data)
 
@@ -42,24 +43,31 @@ class Command(BaseCommand):
                             middleband = round(val.get('middleband'), coin.precision)
                             lowerband = round(val.get('lowerband'), coin.precision)
 
-                            if 21 > rsi > 25:
-                                message = '‼️+ Possibile entry long: ' + coin.symbol + " " \
+                            # Long signal
+                            if rsi < 25 and close <= lowerband:
+                                now = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+                                message = '‼️+ Entry long: ' + coin.symbol + " " \
                                           "\n" + 'Time Frame: ' + str(interval) + \
                                           "\n" + "Candle Close: " + str(close) + \
                                           "\n" + "RSI: " + str(rsi) + \
                                           "\n" + "Upperband: " + str(upperband) + \
                                           "\n" + "Middleband: " + str(middleband) + \
-                                          "\n" + "Lowerband: " + str(lowerband)
+                                          "\n" + "Lowerband: " + str(lowerband) + \
+                                          "\n" + "Date: " + str(now)
                                 telegram.send(message)
 
-                            if 80 < rsi < 87:
-                                message = '‼️+ Possibile entry short: ' + coin.symbol + " " \
+                            # Short signal
+                            if rsi > 80 and close >= upperband:
+                                now = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+                                message = '‼️+  Entry short: ' + coin.symbol + " " \
                                           "\n" + 'Time Frame: ' + str(interval) + \
                                           "\n" + "Candle Close: " + str(close) + \
                                           "\n" + "RSI: " + str(rsi) + \
                                           "\n" + "Upperband: " + str(upperband) + \
                                           "\n" + "Middleband: " + str(middleband) + \
-                                          "\n" + "Lowerband: " + str(lowerband)
+                                          "\n" + "Lowerband: " + str(lowerband) + \
+                                          "\n" + "Date: " + str(now)
+
                                 telegram.send(message)
 
                         ComputedData.objects.create(
