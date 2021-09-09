e4dc02aebc8053f7cc9a22191a50015525401515
kernel_void <egeominotti@gmail.com>
2021-09-09 21:50:57 +0200

    - fix

--- bot/views.py
+++ bot/views.py
@@ -1,17 +1,16 @@
 import datetime
+import json
+
 from django.http import JsonResponse
 from bot.services.telegram import Telegram
 from django.views.decorators.csrf import csrf_exempt
+
 
 @csrf_exempt
 def webhook_tradingview(request):
     telegram = Telegram()
     entry_text = "Test webook"
+    data = json.loads(request.body)
     telegram.send(entry_text)
-    responseData = {
-        'id': 4,
-        'name': 'Test Response',
-        'roles' : ['Admin','User']
-    }
 
-    return JsonResponse(responseData)
+    return JsonResponse(data)
