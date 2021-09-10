b89674427aaaec8000674a953162d9828dad2022
egeominotti <egeominotti@gmail.com>
2021-09-10 15:51:28 +0200

    - fix round profit

--- bot/views.py
+++ bot/views.py
@@ -140,6 +140,7 @@ def trading(id, user, ticker):
         ex = ExchangeHelper(cl, 1)
 
         if id == 'ES':
+
             quantity = ex.get_leveraged_quantity(ticker)
             ex.sell_market_futures(quantity, ticker)
 
@@ -154,6 +155,7 @@ def trading(id, user, ticker):
             r.set(key, json.dumps({"quantity": float(quantity), "start_balance": float(balance)}))
 
         if id == 'EL':
+
             quantity = ex.get_leveraged_quantity(ticker)
             ex.buy_market_futures(quantity, ticker)
 
@@ -168,6 +170,7 @@ def trading(id, user, ticker):
             r.set(key, json.dumps({"quantity": float(quantity), "start_balance": float(balance)}))
 
         if id == 'CS':
+
             value = json.loads(r.get(key))
             ex.buy_market_futures(value.get('quantity'), ticker)
 
@@ -182,6 +185,7 @@ def trading(id, user, ticker):
                          "\nDate: " + str(now)
 
         if id == 'CL':
+
             value = json.loads(r.get(key))
             ex.sell_market_futures(value.get('quantity'), ticker)
 
