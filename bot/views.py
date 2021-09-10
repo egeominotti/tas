5dfefae281da6e189f7bf0fa8cb88b632be45e1c
kernel_void <egeominotti@gmail.com>
2021-09-10 02:34:26 +0200

    - fix

--- bot/views.py
+++ bot/views.py
@@ -26,7 +26,7 @@ class ExchangeHelper:
 
     def get_leveraged_quantity(self, symbol):
 
-        balance_wallet = self.get_current_balance_futures_() * 0.50
+        balance_wallet = self.get_current_balance_futures_() * 0.40
         quantity_precision_live = self.get_symbol_precision(symbol)
         price_coin = self.current_price_coin(symbol)
 
@@ -179,7 +179,6 @@ def webhook_tradingview(request):
             EL -> Entry Long
             CL -> Close Long
         """
-
 
         try:
 
